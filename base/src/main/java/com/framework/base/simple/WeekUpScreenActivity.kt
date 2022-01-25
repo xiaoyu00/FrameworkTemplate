472066b2b552dc02e77f2709be30a0cfbe5f6d31
辰 <1273537654@qq.com>
2022-01-25 10:12:06 +0800

    添加唤醒demo

---
+++ base/src/main/java/com/framework/base/simple/WeekUpScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.framework.base.simple
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.framework.base.R
+
+/**
+ * 锁屏唤醒并解锁屏幕
+ */
+class WeekUpScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 设置点亮屏幕
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                    or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+        )
+        setContentView(R.layout.activity_week_up_screen)
+    }
+
+    public fun startWeekUpScreenActivity() {
+        val intent: Intent = Intent(
+            this@WeekUpScreenActivity,
+            WeekUpScreenActivity::class.java
+        )
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+        startActivity(intent)
+    }
+}
