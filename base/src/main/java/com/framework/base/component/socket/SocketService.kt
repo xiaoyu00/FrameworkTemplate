1dae73d164ea3cb706877bc4f1a68f1928015fcf
辰 <1273537654@qq.com>
2022-01-25 14:03:50 +0800

    socket

--- base/src/main/java/com/framework/base/component/socket/SocketService.kt
+++ base/src/main/java/com/framework/base/component/socket/SocketService.kt
@@ -4,6 +4,9 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 
+/**
+ * 处理其它操作
+ */
 class SocketService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         return null
