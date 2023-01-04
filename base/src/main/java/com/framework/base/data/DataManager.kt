1c7b34e8e1b1808709f02a2a6c8778291c007f66
辰 <1273537654@qq.com>
2023-01-04 16:02:14 +0800

    kotlin 携程

--- base/src/main/java/com/framework/base/data/DataManager.kt
+++ base/src/main/java/com/framework/base/data/DataManager.kt
@@ -1,8 +1,34 @@
 package com.framework.base.data
 
+import com.framework.base.data.userdata.UserData
 import com.framework.base.model.UserInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
+/**
+ * 使用：在mainActivity
+ * private fun initConfigData() {
+        lifecycleScope.launch {
+            DataManager.getLandsAndUpload()
+        }
+    }
+ */
 object DataManager {
     val userListData= ListLiveData<UserInfo>()
+    val userData = UserData()
 
+    suspend fun getLandsAndUpload() = withContext(Dispatchers.IO) {
+        val landList = userData.getLocalLands(userId)
+        if (!landList.isNullOrEmpty()) {
+            landList.forEach { l ->
+                userData.add(l)
+            }
+        }
+    }
+    suspend fun getLands(baseId: Long, hasNet: Boolean): List<Land> {
+        if (hasNet) {
+            userData.getLands(baseId)
+        }
+        return userData.getLocalLands(baseId)
+    }
 }
